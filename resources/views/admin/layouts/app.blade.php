def3a0fbd26ba53186825cf90812aa6448fd3552
ndn2312 <kocoten001a@gmail.com>
2024-06-16 21:47:39 +0700

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -53,7 +53,9 @@
 								<i class="fas fa-user-cog mr-2"></i> Settings								
 							</a>
 							<div class="dropdown-divider"></div>
-							
+							<a href="#" class="dropdown-item">
+								<i class="fas fa-lock mr-2"></i> Change Password
+							</a>
 							<div class="dropdown-divider"></div>
 							<a href="{{route('admin.logout')}}" class="dropdown-item text-danger">
 								<i class="fas fa-sign-out-alt mr-2"></i> Logout							
